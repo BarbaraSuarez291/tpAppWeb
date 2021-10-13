36fa6bacb5e6982301bd2e1e4e052064217b78ad
brianSalinas7 <braiianncr7@gmail.com>
2021-10-12 22:01:21 -0300

    registro funciona

--- registro.php
+++ registro.php
@@ -1,5 +1,7 @@
 <?php
     include("./includes/header.php");
+
+    
 ?>
 
 
@@ -7,26 +9,26 @@
     <section>
       <div class="form-registro">
         <h2>Registrate</h2>
-        <form action="index.php">
+        <form action="index.php" method="POST">
           <div class="nameSignUp">
 
-            <input id="NameSignUp"class="input" type="text" placeholder="Nombre">
+            <input id="NameSignUp"class="input" type="text" placeholder="Nombre" name="nombre">
             <p id="errName"></p>
 
-            <input id="lastNameSignUp" class="input" type="text" placeholder="apellido">
+            <input id="lastNameSignUp" class="input" type="text" placeholder="apellido" name="apellido">
             <p id="errLastName"></p>
 
           </div>
           
-            <input id="emailSignUp" class="input" type="email" placeholder="email">
+            <input id="emailSignUp" class="input" type="email" placeholder="email" name="email">
             <p id="errEmail"></p>
 
           <div class="nameSignUp">
 
-            <input id="passwordSignUp" class="input" type="password" placeholder="password">
+            <input id="passwordSignUp" class="input" type="password" placeholder="password" name="password">
             <p id="errPassword"></p>
 
-            <input id="passwordConfirmSignUp" class="input" type="password" placeholder=" confirme password">
+            <input id="passwordConfirmSignUp" class="input" type="password" placeholder=" confirme password" name="passwordConfirm">
             <p id="errPasswordConfirm"></p>
 
           </div>
@@ -46,7 +48,7 @@
 
           <div class="botones-link fontz-15">
             <a href="login.php">Ya tengo cuenta</a>
-            <button class="activa-link">Registrarse</button>
+            <button class="activa-link" name="signup">Registrarse</button>
           </div>
 
         </form>
